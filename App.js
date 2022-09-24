c4b269615222cdebc776f1cd3b063f4905b6dabd
trongphan <trongtrong099@gmail.com>
2022-09-24 01:48:42 +0100

    chinh sua

--- App.js
+++ App.js
@@ -8,37 +8,39 @@ export default function App() {
         colors={['white', '#40E0D0']}
         start={{x: 0, y: 0.5}}
         end={{x: 1, y: 1}}
-        style={{flex:1,alignItems: 'center'}}
+        style={{flex:1,alignItems: 'center',width:'100%'}}
       >
 
       <View style={{
         marginTop:100,
-        height:140,
-        width:140,
-        border:"15px solid black", 
+        height:200,
+        width:200,
+        borderColor:'black',
+        borderWidth: 20,
         borderRadius:"100%"}}/>
-      <Text style={{fontSize:20, marginTop:50, fontWeight:700,textAlign:"center",  width:155, marginTop:80,}}>GROW<br/>YOUR BUSINESS</Text>
+
+      <Text style={{fontSize:20, marginTop:50, fontWeight:'bold',textAlign:"center",  width:250, marginTop:80,}}>GROW {"\n"}YOUR BUSINESS</Text>
       
-      <Text  style={{width:310,fontWeight:700, textAlign:"center", marginTop:30}}>We will help you to grow your business using
+      <Text  style={{width:310,fontWeight:'bold', textAlign:"center", marginTop:30}}>We will help you to grow your business using
         online server</Text>
 
         
-      <View style={{width:"100%",display:"flex",flexDirection: 'row', alignItems:"stretch", justifyContent:"space-around", marginTop:50}}>
+      <View style={{width:"100%",display:"flex",flexDirection: 'row',  justifyContent:'space-evenly', marginTop:50}}>
       <TouchableOpacity
-        style={{ alignItems: "center", height:40,
+        style={{  height:40,
                 backgroundColor: "#E3C000",
                 padding: 10}}>
-        <Text style={{fontWeight:700}}>LOGIN</Text>
+        <Text style={{fontWeight:'bold'}}>LOGIN</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        style={{ alignItems: "center", height:40,
+        style={{  height:40,
                 backgroundColor: "#E3C000",
                 padding: 10}}>
-        <Text style={{fontWeight:700}}>SIGN UP</Text>
+        <Text style={{fontWeight:'bold'}}>SIGN UP</Text>
       </TouchableOpacity>
       </View>
 
-      <Text  style={{fontWeight:700,marginTop:50}}>
+      <Text  style={{fontWeight:'bold',marginTop:70}}>
         HOW WE WORK?
       </Text>
       <StatusBar style="auto" />
